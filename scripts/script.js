e0260be8a3169920e576efad088777932e220da2
abir-halwa <abeer_h83@hotmail.com>
2020-02-17 12:29:03 -0500

    updated js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,78 +1,88 @@
-const drinkApp={};
+const drinkApp = {};
 drinkApp.drinks;
-drinkApp.apiCall = function (queryString){
+drinkApp.apiCall = function (queryString) {
     $.ajax({
-        url: 'https://api.spoonacular.com/recipes/search',
+        url: 'https://api.spoonacular.com/recipes/complexSearch',
         method: 'GET',
         dataType: 'json',
         data: {
-            app_id: '64e44f2c',
-            app_key: 'a227b3ca7e2072598902a784d880a927',
-            from:0,
-            to:100,
-            q: queryString,
-            dishtype:type
-           
+            apiKey: 'b5a81aab75e347daa78075a3628b5389',
+            type: 'drink',
+            addRecipeInformation: 'true',
+            titleMatch: queryString,
+            number: 100,
         }
+
     }).then(function (result) {
-        drinkApp.drinks=result;
-        console.log(drinkApp.drinks);
+        drinkApp.drinks = result.results;
         drinkApp.showResult();
-       
+
     });
 }
 
-drinkApp.randomizer =function(resultLength){
+drinkApp.getIngredients = function (id) {
+    $.ajax({
+        url: `https://api.spoonacular.com/recipes/${id}/information`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apiKey: 'b5a81aab75e347daa78075a3628b5389',
+        }
+
+    }).then(function (result) {
+        $('.ingredients').empty();
+        result.extendedIngredients.forEach(function (item) {
+            const listItem = `<li>${item.original} </li>`;
+            $('.ingredients').append(listItem);
+        });
+
+    });
+}
+
+drinkApp.randomizer = function (resultLength) {
     const selectedDrinkIndex = Math.floor(Math.random() * resultLength);
     return selectedDrinkIndex;
 }
-drinkApp.removeDrink=function ( index){
-    
-    // console.log(index);
-    drinkApp.drinks.hits.splice(index,1);
-   
+drinkApp.removeDrink = function (index) {
+    drinkApp.drinks.splice(index, 1);
+
 }
-drinkApp.anotherOptionEventListener=function(hits){
-    $('#anotherDrinkButton').on ('click',function(){
+drinkApp.anotherOptionEventListener = function () {
+    $('#anotherDrinkButton').on('click', function () {
         drinkApp.showResult()
     });
 
 }
 
 
-drinkApp.showResult =function(){
-    const selectedDrinkIndex = drinkApp.randomizer(drinkApp.drinks.hits.length);
+drinkApp.showResult = function () {
+    const selectedDrinkIndex = drinkApp.randomizer(drinkApp.drinks.length);
     $('.moodDiv').addClass('hidden');
     $('.resultDiv').removeClass('hidden');
-    $('#drinkTitle').text(drinkApp.drinks.hits[selectedDrinkIndex].recipe.label);
-    $('#drinkImage').attr('src', drinkApp.drinks.hits[selectedDrinkIndex].recipe.image);
-    $('#drinkImage').attr('alt', drinkApp.drinks.hits[selectedDrinkIndex].recipe.label);
-    $('.ingredients').empty();
-    drinkApp.drinks.hits[selectedDrinkIndex].recipe.ingredients.forEach(function(item){
-        const listItem =`<li>${item.text}</li>`;
-        $('.ingredients').append(listItem);
-    });
+    $('#drinkTitle').text(drinkApp.drinks[selectedDrinkIndex].title);
+    $('#drinkImage').attr('src', drinkApp.drinks[selectedDrinkIndex].image);
+    $('#drinkImage').attr('alt', drinkApp.drinks[selectedDrinkIndex].title);
+    drinkApp.getIngredients(drinkApp.drinks[selectedDrinkIndex].id);
+    $('#drinkLink').attr('href', drinkApp.drinks[selectedDrinkIndex].sourceUrl);
+    drinkApp.removeDrink(selectedDrinkIndex);
 
-    $('#drinkLink').attr('href', drinkApp.drinks.hits[selectedDrinkIndex].recipe.url);
-   drinkApp.removeDrink( selectedDrinkIndex);
-    
 
 }
 
-drinkApp.eventListener=function(){
-    $('#submitButton').on('click', function(e){
+drinkApp.submitEventListener = function () {
+    $('#submitButton').on('click', function (e) {
         e.preventDefault();
-       const mood =$('input[type=radio]:checked').val();
-            drinkApp.apiCall(mood);
-        
+        const mood = $('input[type=radio]:checked').val();
+        drinkApp.apiCall(mood);
+
     });
 }
 
-drinkApp.init=function (){
-drinkApp.eventListener();
-drinkApp.anotherOptionEventListener();
-    
+drinkApp.init = function () {
+    drinkApp.submitEventListener();
+    drinkApp.anotherOptionEventListener();
+
 }
-$(function(){
+$(function () {
     drinkApp.init();
 })
